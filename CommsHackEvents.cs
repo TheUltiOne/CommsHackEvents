5b2300ae8e0988bb73254cedd83828024fe44f7f
TheUltiOne <75040208+TheUltiOne@users.noreply.github.com>
2020-12-22 12:17:18 -0500

    Update CommsHackEvents.cs

--- CommsHackEvents.cs
+++ CommsHackEvents.cs
@@ -1,7 +1,9 @@
-﻿using Exiled.API.Features;
+using Exiled.API.Features;
 using Exiled.API.Enums;
 
 using Server = Exiled.Events.Handlers.Server;
+using Player = Exiled.Events.Handlers.Player;
+using Warhead = Exiled.Events.Handlers.Warhead;
 
 using System;
 
@@ -33,11 +35,52 @@ namespace CommsHackEvents
         public void RegisterEvents()
         {
             events = new EventHandlers();
-            
+            Server.WaitingForPlayers += events.OnWaitingForPlayers;
+            Server.RoundStarted += events.OnRoundStarted;
+            Server.EndingRound += events.OnEndingRound;
+            Server.RestartingRound += events.OnRestartingRound;
+            Server.RespawningTeam += events.OnRespawningTeam;
+
+            Player.Joined += events.OnJoined;
+            Player.Left += events.OnLeft;
+            Player.EnteringFemurBreaker += events.OnEnteringFemurBreaker;
+            Player.Dying += events.OnDying;
+            Player.Hurting += events.OnHurting;
+            Player.ActivatingWarheadPanel += events.OnActivatingWarheadPanel;
+            Player.InsertingGeneratorTablet += events.OnInsertingGeneratorTablet;
+            Player.EjectingGeneratorTablet += events.OnEjectingGeneratorTablet;
+            Player.FailingEscapePocketDimension += events.OnFailingEscapePocketDimension;
+            Player.EscapingPocketDimension += events.OnEscapingPocketDimension;
+            Player.Escaping += events.OnEscaping;
+            Player.Kicked += events.OnKicked;
+            Player.Banned += events.OnBanned;
+
+            Warhead.Detonated += events.OnDetonating;
         }
 
         public void UnregisterEvents()
         {
+            Server.WaitingForPlayers -= events.OnWaitingForPlayers;
+            Server.RoundStarted -= events.OnRoundStarted;
+            Server.EndingRound -= events.OnEndingRound;
+            Server.RestartingRound -= events.OnRestartingRound;
+            Server.RespawningTeam -= events.OnRespawningTeam;
+
+            Player.Joined -= events.OnJoined;
+            Player.Left -= events.OnLeft;
+            Player.EnteringFemurBreaker -= events.OnEnteringFemurBreaker;
+            Player.Dying -= events.OnDying;
+            Player.Hurting -= events.OnHurting;
+            Player.ActivatingWarheadPanel -= events.OnActivatingWarheadPanel;
+            Player.InsertingGeneratorTablet -= events.OnInsertingGeneratorTablet;
+            Player.EjectingGeneratorTablet -= events.OnEjectingGeneratorTablet;
+            Player.FailingEscapePocketDimension -= events.OnFailingEscapePocketDimension;
+            Player.EscapingPocketDimension -= events.OnEscapingPocketDimension;
+            Player.Escaping -= events.OnEscaping;
+            Player.Kicked -= events.OnKicked;
+            Player.Banned -= events.OnBanned;
+
+            Warhead.Detonated -= events.OnDetonating;
             events = null;
         }
     }
